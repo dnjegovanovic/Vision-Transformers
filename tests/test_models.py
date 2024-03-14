3f9a4f08e7f0f0aabb71c244c9fc3122256fbc4d
dusannj <dusan.njegovnaovic95@gmail.com>
2024-03-14 23:50:35 +0100

    black formater

--- tests/test_models.py
+++ tests/test_models.py
@@ -6,13 +6,20 @@ import torch
 from vitapp.config.core import config
 
 print(config)
+
+
 def test_patch_embeddings():
     config_data = config.model_conf.ViT
 
     patch_emb = PatchEmbeddings(config_data)
 
     input_x = torch.rand(
-        [8, config_data["num_channels"], config_data["image_size"], config_data["image_size"]]
+        [
+            8,
+            config_data["num_channels"],
+            config_data["image_size"],
+            config_data["image_size"],
+        ]
     )
 
     print(f"Input tensor shape: {input_x.shape}")
@@ -28,11 +35,20 @@ def test_position_emb():
     pos_emb = PositionEmbedding(config_data)
 
     input_x = torch.rand(
-        [8, config_data["num_channels"], config_data["image_size"], config_data["image_size"]]
+        [
+            8,
+            config_data["num_channels"],
+            config_data["image_size"],
+            config_data["image_size"],
+        ]
     )
 
     print(f"Input tensor shape: {input_x.shape}")
 
     res = pos_emb(input_x)
     print(f"Output tensor: {res.shape}")
-    assert res.shape == (8, pos_emb.patch_emb.num_patches + 1, config_data["hidden_size"])
+    assert res.shape == (
+        8,
+        pos_emb.patch_emb.num_patches + 1,
+        config_data["hidden_size"],
+    )
